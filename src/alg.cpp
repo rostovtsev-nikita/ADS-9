d0ec4fb2c7c21abaa131bb87972e444d7cd4681e
rostovtsev-nikita <99068528+rostovtsev-nikita@users.noreply.github.com>
2022-05-11 19:02:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,29 +7,5 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
-  std::ifstream file(filename);
-  BST<std::string> tree;
-  std::string word, line;
-  while (!file.eof()) {
-    file >> word;
-    int i = 0;
-    while (i < word.length()) {
-      int k = 0;
-      while ((word[i] >= 'a' && word[i] <= 'z' ||
-              word[i] >= 'A' && word[i] <= 'Z') && i < word.length()) {
-        if (word[i] >= 'A' && word[i] <= 'Z')
-          word[i] += 32;
-        line += word[i];
-        k++;
-        i++;
-      }
-      if (k != 0) {
-        tree.add(line);
-        line = "";
-      } else {
-        i++;
-      }
-    }
-  }
-  return tree;
+
 }
